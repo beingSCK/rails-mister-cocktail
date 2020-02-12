9e3f5b2a2d36a37fb2841b8090c216b477cbb2c9
Sean Keegan <sean@das.llc>
2020-02-12 16:21:22 +0200

    Passing cocktails 'get' actions - still more controller work to do !

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,22 @@
 class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.save
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require[:dose].permit[:cocktail_id, :description]
+  end
 end
