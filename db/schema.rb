ab557185de5ddae362fa1136c465bcbf62861919
Sean Keegan <sean@das.llc>
2020-02-12 15:42:03 +0200

    fixed schema + added model validations + added controllers for doses and cocktails

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_02_12_100413) do
   end
 
   create_table "doses", force: :cascade do |t|
+    t.string "description"
     t.bigint "cocktail_id"
     t.bigint "ingredient_id"
     t.datetime "created_at", null: false
