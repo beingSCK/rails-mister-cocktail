8cabf9f783b2f8e088b332e12351bcfc633d0587
Sean Keegan <sean@das.llc>
2020-02-13 18:20:54 +0200

    fixed crash on delete dose

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -20,6 +20,8 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
+
+    redirect_to cocktail_path(params[:cocktail_id])
   end
 
   private
